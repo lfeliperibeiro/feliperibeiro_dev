b573c2a35af85768af6124d92996fb28b32482f4
Felipe Ribeiro <piporibeiro@gmail.com>
2022-10-01 17:42:45 -0300

    chore: add dracula ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import 'dracula-ui/styles/dracula-ui.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
