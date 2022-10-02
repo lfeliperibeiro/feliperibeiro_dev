bb72fefa29b10c260b260b723a0443bef197271d
Felipe Ribeiro <piporibeiro@gmail.com>
2022-10-02 01:10:57 -0300

    refactor: adjust text ad profile

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,23 +2,27 @@ import { Repositories } from '../Repositories/Repositories';
 import { Skills } from '../Skills';
 import { Social } from '../Social/Social';
 import { IntroInfo } from '../IntroInfo/IntroInfo';
-import {Avatar, Box, Heading} from "dracula-ui";
-
+import { Avatar, Box, Heading } from 'dracula-ui';
+import profileImg from '../../assets/profile.png';
 export const Hero = () => {
   return (
     <Box color="blackSecondary">
-      <div className={'flex flex-col m-2 lg:p-10'}>
-        <Avatar
-          title="Felipe RIbeiro"
-          src="https://github.com/lfeliperibeiro.png"
-          height={100}
-          width={100}
-        />
+      <div className={'flex mt-8 flex-col m-2 lg:p-10 items-center'}>
+        <div className={'items-center'}>
+          <Avatar
+            title="Felipe RIbeiro"
+            src={profileImg.src}
+            height={100}
+            width={100}
+            borderVariant="large"
+          />
+        </div>
+
         <IntroInfo />
         <Social />
         <div className={'mb-4'}>
-          <div className={'ml-2 mb-4'}>
-            <Heading size="lg" color="pink" >
+          <div className={'text-center mb-4'}>
+            <Heading size="lg" color="pink">
               Repositories
             </Heading>
           </div>
