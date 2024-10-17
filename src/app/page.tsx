d38e432c0d3548442e098d931639a9c7e3fca68f
Felipe Ribeiro <piporibeiro@gmail.com>
2024-10-17 11:41:46 -0300

    feat: add SEO and pagespeed data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,95 @@
-'use client'
-
 import { Footer } from '../components/Footer'
 import { Hero } from '../components/Hero'
-import { useState } from 'react'
+import Head from 'next/head'
 
-export default function Home() {
-  const [language, setLanguage] = useState('en')
+// Define the interface for the PageSpeed data
+interface LighthouseCategory {
+  score: number
+}
 
-  const toggleLanguage = () => {
-    setLanguage((prevLanguage) => (prevLanguage === 'en' ? 'pt' : 'en'))
+interface LighthouseResult {
+  categories: {
+    performance: LighthouseCategory
+    accessibility: LighthouseCategory
+    'best-practices': LighthouseCategory
+    seo: LighthouseCategory
+  }
+}
+
+interface PageSpeedData {
+  lighthouseResult: LighthouseResult
+}
+
+export default async function Home() {
+  let pageSpeedData: PageSpeedData | null = null
+
+  try {
+    const response = await fetch(
+      `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=https://feliperibeiro-dev.vercel.app&key=AIzaSyD3KXs0SqHe3W-5apfPaz0iyeTmRKtj3Ao&category=performance&category=accessibility&category=best-practices&category=seo`,
+    )
+    if (!response.ok) {
+      throw new Error('Network response was not ok')
+    }
+    pageSpeedData = await response.json()
+  } catch (error) {
+    console.error('Failed to fetch PageSpeed data:', error)
   }
 
   return (
-    <main className="flex h-screen w-full flex-col items-end overflow-auto bg-gray-950">
-      <div>
-        <button
-          className="p-4 text-slate-400 hover:text-slate-200"
-          onClick={toggleLanguage}
-        >
-          {language === 'en' ? 'Portuguese' : 'English'}
-        </button>
-      </div>
+    <>
+      <Head>
+        <title>Felipe Ribeiro - Senior Frontend Engineer</title>
+        <meta
+          name="description"
+          content={
+            'Perfil profissional de Felipe Ribeiro como Engenheiro Frontend Sênior.'
+          }
+        />
+        <meta
+          name="keywords"
+          content="Frontend Engineer, React, Next.js, Typescript, CI/CD, Marketing, Programming, Web Development"
+        />
+        <meta name="author" content="Felipe Ribeiro" />
+      </Head>
+      <main className="flex h-screen w-full flex-col items-end overflow-auto bg-gray-950">
+        {pageSpeedData && (
+          <div className="mt-14 flex w-full flex-1 flex-col items-center justify-center pl-8 sm:pl-0">
+            <div className="mx-auto mt-10 w-full max-w-xl flex-1 flex-col items-start justify-center">
+              <h2 className="mb-2 text-xl text-zinc-50">PageSpeed Insights</h2>
+              <p className="text-left text-base font-light text-zinc-400">
+                Performance Score:{' '}
+                <span className="text-green-500">
+                  {pageSpeedData.lighthouseResult.categories.performance.score *
+                    100}
+                </span>
+              </p>
+              <p className="text-left text-base font-light text-zinc-400">
+                Accessibility Score:{' '}
+                <span className="text-green-500">
+                  {pageSpeedData.lighthouseResult.categories.accessibility
+                    .score * 100}
+                </span>
+              </p>
+              <p className="text-left text-base font-light text-zinc-400">
+                Best Practices Score:{' '}
+                <span className="text-green-500">
+                  {pageSpeedData.lighthouseResult.categories['best-practices']
+                    .score * 100}
+                </span>
+              </p>
+              <p className="text-left text-base font-light text-zinc-100">
+                SEO Score:{' '}
+                <span className="text-green-500">
+                  {pageSpeedData.lighthouseResult.categories.seo.score * 100}
+                </span>
+              </p>
+            </div>
+          </div>
+        )}
 
-      <Hero language={language} setLanguage={setLanguage} />
-      <Footer />
-    </main>
+        <Hero />
+        <Footer />
+      </main>
+    </>
   )
 }
