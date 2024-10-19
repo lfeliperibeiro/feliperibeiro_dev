ada094b32a4c0d16ab12ef0f572d3e4e315b8f70
Felipe Ribeiro <piporibeiro@gmail.com>
2024-10-19 19:23:45 -0300

    refactor: change name file hero to content

--- src/components/hero.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { Hero } from './Hero';
-
-describe('Hero', () => {
-  it('should render the hero component', () => {
-    render(<Hero />);
-    expect(screen.getByTestId('hero')).toBeInTheDocument();
-  });
-});
